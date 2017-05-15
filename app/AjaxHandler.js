690b58b5b231e191bf88177c4aeafa9ec1d4bbb8
Alan Wong <leungasdeveloper@gmail.com>
2017-05-15 18:24:42 +0800

    AjaxHandler

--- app/AjaxHandler.js
+++ app/AjaxHandler.js
@@ -1,3 +1,14 @@
+import {API_HOST} from './config.js';
 
 export default class AjaxHandler {
+    
+	constructor (jQuery) {
+		this.jQuery = jQuery;
+	}
+
+    ajaxGet(path, data){
+        jQuery.get(API_HOST + path, data, function(res){
+            return res;
+        });
+    }
 }
