3806920ebb54a167cfa7055c99bb22f015086691
Pierre Laurent <pierre.laurent@ve.com>
2017-05-15 18:38:20 +0800

    index update

--- app/app.main.js
+++ app/app.main.js
@@ -3,5 +3,12 @@ import AjaxHandler from './AjaxHandler.js';
 
 (function() {
 
+	$("#submitId").onclick(function(){
+		var numberId = $("#numberId");
+        ajaxHandler.ajaxGet("/api/customer/name", {cid: numberId}, function( result ){ 
+    		$("#nameresult").html(result)
+        })
+    });
+
 })();
 
